0090ddb162a08e8361e3df8044f0670155da8758
mylasampang <mm_sampang.student@ua.edu.ph>
2022-08-23 22:14:44 +0800

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -10,28 +10,11 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-        I am Alaina Louise G. Payawal, 22 years old, from Candaba, Pampanga. 
-        I remember one time, my friend told me to put my energy into things that help me grow - I am trying to do that. 
-        I am trying to create a life that makes me happy. I love to travel and experience different cultures of every place that I have been.
-        I love my coffee black, old school, right?
+       I am Myla Manalastas Sampang. I am 20 years old and turning 21 this coming month of september. I am from San Jaun, San Luis Pampanga. I like playing/watching  volleyball, and I like to sing and dance even though Im not that totally good at it. 
+       I have a twin sister and we are the eldest among our siblings. I take the IT course because I had a little bit of knowledge about technology and because it is indemand in todays time.
+       A vision Ive always wanted for myself 10 years from now is to be a wealthy business woman working in a big company in a better position. And to be the best version of myself and do what I really like, 
+       following my profession and making my family proud. 
         </p>
-
-        <p>
-        People often call me Jack of all Trades but that’s far beyond the truth, I consider myself as mediocre.
-        </p>
-        <p>
-        Despite all the hate and chaos happening in our world right now, I still believe in the goodness of people.
-        I personally believe that we should have a healthy relationship with other people. 
-        It is so nice to connect with people emotionally and somehow join them on a journey that might change you as you 
-        follow them and draws you into a story that is bigger than yourself.
-        </p>
-
-        <p>
-        Whenever I feel stressed out, I like to play the piano & guitar, and to draw, too. 
-        I like to allot time to sit with my thoughts and feel my feelings - it’s like going to another universe, I enjoy silence once in a while.
-
-        </p>
-
         
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
